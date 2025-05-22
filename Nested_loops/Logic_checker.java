5edba790a09c9aebce7e9f7f8c488fed025a7cb0
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-05-23 05:14:24 +0530

    ok

---
+++ Nested_loops/Logic_checker.java
@@ -0,0 +1,12 @@
+package Nested_loops;
+
+public class Logic_checker {
+    public static void main(String[] args) {
+        for(int i = 1; i<=2;i++){
+            for(int j = 1 ; j <=3;j++){
+                System.out.print(" Hello ");
+            }
+            System.out.println();
+        }
+    }
+}
